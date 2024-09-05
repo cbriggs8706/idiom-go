871706feec1b01c76a89e255a594bc59dc171ec2
cbriggs8706 <cameronbriggs8706@gmail.com>
2024-09-05 15:19:44 -0600

    manual header added

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,32 @@ export default function RootLayout({
 	return (
 		<ClerkProvider>
 			<html lang="en">
+				<head>
+					<link rel="icon" href="/favicon.ico" />
+					<link
+						rel="android-chrome"
+						sizes="192x192"
+						href="/android-chrome-192x192.png"
+					/>
+					<link
+						rel="android-chrome"
+						sizes="512x512"
+						href="/android-chrome-512x512.png"
+					/>
+					<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+				</head>
 				<body className={font.className}>
 					<Toaster />
 					<ExitModal />
