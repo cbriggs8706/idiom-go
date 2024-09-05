6a0fb6ddb4e26968044a3abb5e55b7acde934f14
cbriggs8706 <cameronbriggs8706@gmail.com>
2024-09-04 18:21:44 -0600

    test page

---
+++ app/(main)/test/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function page() {
+	return (
+		<div>
+			<iframe
+				style={{ maxWidth: '100%' }}
+				src="https://wordwall.net/embed/e5152eda9b53426880184aad3211391e?themeId=2&templateId=72&fontStackId=0"
+				width="500"
+				height="380"
+				frameBorder={'0'}
+				allowFullScreen
+			></iframe>
+			<iframe
+				allow="fullscreen; autoplay;"
+				allowFullScreen
+				width="795"
+				height="690"
+				frameBorder="0"
+				src="https://www.educaplay.com/game/20192081-testing.html"
+			></iframe>
+		</div>
+	)
+}
+
+export default page
